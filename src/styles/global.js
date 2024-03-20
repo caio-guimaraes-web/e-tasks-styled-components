279d3ad2dedab468bfc71e77bf6035ba37631358
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-03-20 18:14:23 -0300

    fixing name file and priorities responsivity

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components"
-import { DEVICE_BREAKPOINTS } from "./deviceBreakpoint"
+import { DEVICE_BREAKPOINTS } from "./deviceBreakpoints"
 
 export default createGlobalStyle`
   * {
